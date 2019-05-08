ab29caaf021b0004e4b6cf44afb168203b597d3c
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-08 11:21:03 +0100

    bootstrapped

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
 import Main from './components/main';
 
 
 class App extends Component {
   render() {
     return (        
-      <div>        
+      <Container>        
         <Main />
-      </div>
+      </Container>
     );
   }
 }

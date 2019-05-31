daac535a9fd0e3a421117fee11a27887f428a096
Tiago Cunha <tcunha_lp@hotmail.com>
2019-05-31 12:28:43 +0100

    edit on resp from server

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { ActionCableConsumer } from 'react-actioncable-provider';
+import Auth from '../modules/auth';
+import '../css/chat.css';
 
 class Chat extends Component {
 
@@ -8,44 +10,69 @@ class Chat extends Component {
 		super();		
 		this.state = {
 			conversation_id: "",
-			message: {},
-			response: []
+			conversation: [],
+			message: {
+				body:""
+			}
 		};
 		this.handleReceived = this.handleReceived.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 		this.handleNewMessage = this.handleNewMessage.bind(this);
+		this.chatbox = React.createRef()
+
 	}
 
-	componentWillMount(){
-		if (this.props.sender_id === this.props.recipient_id) {
-			console.log("standing by...")
-		} else {
-			this.handleConversation()
+	componentWillMount(){		
+		if (this.props.sender_id !== this.props.recipient_id) {			
+			const conversation = {request_id: this.props.request_id, sender_id: this.props.sender_id, recipient_id: this.props.recipient_id};
+			this.handleConversation(conversation)
+		};	
+	}
+
+	componentDidUpdate(prevProps) {	
+		if (prevProps.selected !== this.props.selected) {
+			if (this.props.sender_id === this.props.recipient_id) {
+				const conversation = {request_id: this.props.request_id, sender_id: this.props.sender_id, recipient_id: this.props.selected};
+				this.handleConversation(conversation)
+			};	
 		}
+		this.scrollToBottom()
+
 	}
 
-	handleConversation() {
-		const conversation = {request_id: this.props.request_id, sender_id: this.props.sender_id, recipient_id: this.props.recipient_id}
+	handleConversation(conversation) {
+		
 		fetch('http://localhost:3001/conversations', { 
        		method: 'POST', 
         	body: JSON.stringify(conversation), 
         	headers: {
-	        'Content-Type': 'application/json'
+	        	'Content-Type': 'application/json',
+	        	token: Auth.getToken(),
+				'Authorization': `Token ${Auth.getToken()}`
 	      	}
 	    })
 	    .then(res => res.json())
 	    .then(json => {
 	    	this.setState({
-	    		conversation_id: json.conversation_id
+	    		conversation_id: json.id,
+	    		conversation: json.messages
 	    	})
 	    })
 	}
 
+	scrollToBottom(){
+   		var chatbox = this.chatbox; 
+   		if (chatbox.current !== null ) {		
+			chatbox.current.scrollTop = chatbox.current.scrollHeight
+   		}
+	}
+
 	handleReceived(response) {
-		console.log("response",response);
-		this.setState({		
-				response			
-		});
+		this.setState({	
+				conversation_id: response.id,
+				conversation: response.messages			
+		});		
+		
     }
 
     handleChange(e){
@@ -64,16 +91,30 @@ class Chat extends Component {
        		method: 'POST', 
         	body: JSON.stringify(this.state.message), 
         	headers: {
-	        'Content-Type': 'application/json'
+	        	'Content-Type': 'application/json',
+	        	token: Auth.getToken(),
+				'Authorization': `Token ${Auth.getToken()}`
 	      	}
 	    })
 	    .then(res => res.json())
 	    .then(json => {console.log("done")})
+	    e.target.reset();
+	    this.setState({message: {body: ""}})
 	      
     }
 
-	render() {	
-		console.log("res",this.state.response)
+    setUserMessageLayout = (id) => {
+    	let clss = "";
+    	if (id === this.props.sender_id) {
+    		clss = "ml-auto out"
+    	} else {
+    		clss = "mr-auto in"
+    	}
+    	return clss
+    }
+
+	render() {			
+		const messages = this.state.conversation;		
 		return(
 			<Row>
 				<ActionCableConsumer
@@ -82,24 +123,28 @@ class Chat extends Component {
            		>		       				
        			</ActionCableConsumer>
        			<Col>
-	       			<div className="vh-50">
-	       			{this.state.response.map(message => (
-	       					<div>{message.body}</div>
-	       				))}
-	       			</div>
-	       			<Form onSubmit={this.handleNewMessage}>
-					  <Form.Group as={Row} controlId="formPlaintext">
-					    <Form.Label column sm="2">
-					      Message
-					    </Form.Label>
-					    <Col sm="10">
-					      <Form.Control type="text" placeholder="write here" name="body" value={this.state.message.body} onChange={this.handleChange}/>
-					    </Col>
-					  </Form.Group>
-					  <Button variant="primary" type="submit">
-					    Send
-					  </Button>
-					</Form>
+       				<div className="p-2">
+		       			<div ref={this.chatbox} className="max-vh-25 overflow-auto chatbox">
+		       			{messages !== undefined &&
+		       				messages.map(message => (
+		       					<div key={message.id} className="d-flex ">	       						
+		       						<div className={`${this.setUserMessageLayout(message.user_id)} p-2 my-2 message`}>{message.body}</div>
+		       					</div>
+		       				))}
+		       			</div>
+		       			<Form onSubmit={this.handleNewMessage}>
+						  <Form.Row>
+						    <Col xs={10}>
+						      <Form.Control type="text" placeholder="Write message here" name="body" value={this.state.message.body} onChange={this.handleChange}/>
+						   	</Col>
+						  	<Col xs={2}>
+							  <Button variant="primary" type="submit">
+							    Send
+							  </Button>
+							</Col>
+						  	</Form.Row>
+						</Form>
+					</div>
 				</Col>
 
 

45b31e10d11d91e9fdafc3b94226a79399a73a53
Tiago Cunha <tcunha_lp@hotmail.com>
2019-06-07 14:10:12 +0100

    wss

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://friendlyneighbourserver.herokuapp.com';
-export const API_WS_ROOT = 'ws://friendlyneighbourserver.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://friendlyneighbourserver.herokuapp.com/cable';
 //export const API_ROOT = 'http://localhost:3001'
 //export const API_WS_ROOT = 'ws://localhost:3001/cable'
 
